277f919ade471b3dc4d568554dc2ecd8c1ab12ed
Pradhan <Pradhan@192.168.2.5>
2017-06-02 01:39:45 +0530

    as

---
+++ Sub_Sales/src/com/sunit/Main.java
@@ -0,0 +1,20 @@
+package com.sunit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data=new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("hello happy sales people");
+		System.out.println("This app shoes sales data");
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		
+	}
+
+}
