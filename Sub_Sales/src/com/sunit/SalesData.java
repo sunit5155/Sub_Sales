bdb6f87974fba9debdf946cf0defc8e198c6b33f
Pradhan <Pradhan@192.168.2.5>
2017-06-02 02:06:49 +0530

    Resetting output

--- Sub_Sales/src/com/sunit/SalesData.java
+++ Sub_Sales/src/com/sunit/SalesData.java
@@ -4,7 +4,7 @@ public class SalesData {
 	int[] data={0,4,42};
 	public void display(){
 		for (int i = 0; i < data.length; i++) {
-			System.out.println("");
+			System.out.println("Next data :"+data[i]);
 		}
 	}
 
